90ce554904bf6b8cebd365dd1d4ec92c85c183e3
Jimmy Smith <jaystansmith@gmail.com>
2018-10-13 12:38:57 -0600

    Working deletion

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,10 @@
+class ReviewsController < ApplicationController
+
+  def destroy
+    @reviews = Review.find(params[:id])
+    @user = Review.find(params[:id]).user_id
+    @reviews.destroy
+    redirect_to user_path(@user)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "books#index"
   resources :books
-  resources :users 
+  resources :users
   resources :authors
+  resources :reviews
+  # delete "reviews/:id" => "reviews#delete", as: "del"
 end
