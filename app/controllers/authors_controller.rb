e9802bd568d10886067ad79851275a17dd0ce86d
Jenn Lao <jenn.p.lao@gmail.com>
2018-10-14 20:40:26 -0600

    refactors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,7 +3,6 @@ class AuthorsController < ApplicationController
   def show
     @authors = Author.find(params[:id])
     @author_books = @authors.books
-    @book_reviews = @authors.book_reviews
   end
 
 end
