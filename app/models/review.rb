1bf7cd8d7ba4f4927c7852b0e6eda8a5f4fa7170
Jenn Lao <jenn.p.lao@gmail.com>
2018-10-14 22:24:16 -0600

    links all user names

--- app/models/review.rb
+++ app/models/review.rb
@@ -36,12 +36,24 @@ class Review < ApplicationRecord
     end
   end
 
-  def self.top_review_user
+  def self.top_review_user_name
     if self.all.empty?
-      'No reviews'
+      "No reviews"
     else
       order(score: :desc).first.user.name
     end
+  end
+
+  def self.top_review
+    if self.all.empty?
+      0
+    else
+      order(score: :desc).first.user.id
+    end
+  end
+
+  def user_name
+    user.name
   end
 
 end
