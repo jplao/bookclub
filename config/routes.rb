e6f0e2711c7bd6b279cabdcf8f7877bdc311a0cb
Jimmy Smith <jaystansmith@gmail.com>
2018-11-25 21:20:12 -0700

    Reworked the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   root "books#index"
   get '/books', to: 'books#index'
+  get '/books/new', to: 'books#new'
   get '/books/:id', to: 'books#show', as: 'book'
   delete '/books/:id', to: 'books#destroy'
-  get '/books/new', to: 'books#new'
   post '/books', to: 'books#create', as: 'new_book'
+
   get 'users/:id', to: 'users#show', as: 'user'
+
   get 'authors/:id', to: 'authors#show', as: 'author'
-  delete 'authors/:id', to: 'authors#destroy', as: 'review'
-  delete 'reviews/:id', to: 'reviews#destroy'
+  delete 'authors/:id', to: 'authors#destroy'
+
   get 'reviews/new', to: 'reviews#new', as: 'new_review'
+  delete 'reviews/:id', to: 'reviews#destroy', as: 'review'
   post 'reviews/', to: 'reviews#create'
 end
